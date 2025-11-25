e1d88ee0e3c5cd2e280b1efbba2b6359b08aac8d
QaysAlnajjad <qaysalnajjad317@gmail.com>
2025-11-26 01:08:43 +0400

    Initial commit: AWS Multi-Region WordPress DR architecture

--- modules/acm/main.tf
+++ modules/acm/main.tf
@@ -7,7 +7,9 @@
 # ----------------------------------------------------------------------
 resource "aws_acm_certificate" "cert" {
   domain_name               = var.domain_name
-  subject_alternative_names = var.subject_alternative_names
+  subject_alternative_names = [
+    for san in var.subject_alternative_names : trimsuffix(san, ".")
+  ]
   validation_method         = "DNS"
 
   lifecycle {
